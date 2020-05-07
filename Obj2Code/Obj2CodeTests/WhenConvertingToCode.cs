bab19e1e30ab5d8f23b731ecdbf0457c307c6e3e
Philippe Castonguay <philippe.castonguay@034gc.onmicrosoft.com>
2020-05-07 16:46:16 -0400

    added test framework

--- Obj2Code/Obj2CodeTests/WhenConvertingToCode.cs
+++ Obj2Code/Obj2CodeTests/WhenConvertingToCode.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Obj2CodeTests
 {
-    public class Class1
+    public class WhenConvertingToCode
     {
+        [Fact]
+        public void test()
+        {
+
+        }
     }
 }
