01154d32e48f47ed9f1d1e36747c66f0ac409b81
Philippe Castonguay <philippe.castonguay@034gc.onmicrosoft.com>
2020-05-07 17:36:58 -0400

    got ints, strings, and mostly times working

---
+++ Obj2Code/Obj2Code/Obj2Coder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obj2Code
+{
+    public class Obj2Coder
+    {
+        public static string ToCode(object classToConvert)
+        {
+            Type typeToConvert = classToConvert.GetType();
+            if (HasConstructor(typeToConvert))
+            {
+                StringBuilder s = new StringBuilder();
+
+                s.Append("new ");
+                s.Append(typeToConvert.Name.ToString());
+                s.Append("()");
+                s.Append(" ");
+                s.Append("{ ");
+
+                PropertyInfo[] properties = typeToConvert.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                foreach (var property in properties)
+                {
+                    if (property.CanRead && property.CanWrite)
+                    {
+                        var mget = property.GetGetMethod(false);
+                        var mset = property.GetSetMethod(false);
+                        if (mset != null && mget != null)
+                        {
+                            s.Append(property.Name);
+                            s.Append(" = ");
+                            var value = mget.Invoke(classToConvert, null);
+                            var methodType = value.GetType();
+                            if (methodType.Equals(typeof(string)))
+                            {
+                                s.Append("\"");
+                                s.Append(value);
+                                s.Append("\"");
+                            }
+                            else if (methodType.Equals(typeof(DateTime)))
+                            {
+                                DateTime dateTime = (DateTime)value;
+                                s.Append("DateTime.Parse(\"");
+                                s.Append(dateTime.ToString("u"));
+                                s.Append("\")");
+                            }
+                            else
+                            {
+                                s.Append(value);
+                            }
+                            s.Append(", ");
+                        }
+                    }
+                }
+
+                s.Append("}");
+                s.Append(";");
+                return s.ToString();
+            } else
+            {
+                throw new Exception("Your class should have a default constructor");
+            }
+        }
+
+
+        private static bool HasConstructor(Type typeToConvert)
+        {
+            return typeToConvert.GetConstructor(Type.EmptyTypes) != null;
+        }
+    }
+}
--- Obj2Code/Obj2CodeTests/WhenConvertingToCode.cs
+++ Obj2Code/Obj2CodeTests/WhenConvertingToCode.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using Obj2CodeTests.TestClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Obj2Code;
+using Should;
 
 namespace Obj2CodeTests
 {
     public class WhenConvertingToCode
     {
         [Fact]
-        public void test()
+        public void Should_Convert_To_Code()
         {
-
+            var easyClass = new EasyClass() { Id = 5, Name = "Phil", StartDate = DateTime.Parse("2009-06-15 13:45:30Z"), };
+            var result = Obj2Coder.ToCode(easyClass);
+            result.ShouldEqual("new EasyClass() { Id = 5, Name = \"Phil\", StartDate = DateTime.Parse(\"2009-06-15 13:45:30Z\"), };");
         }
     }
 }
